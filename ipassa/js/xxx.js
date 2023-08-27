ed6247efa098988e81037f0699a14b2a381341f2
abolfazl <abolfazl>
2023-08-28 00:50:04 +0330

    's'

---
+++ ipassa/js/xxx.js
@@ -0,0 +1,11 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: "en",
+
+        includedLanguages: 'ar,fa,fr,ru,es,en,zh-CN,ko,pt,ja,de',
+
+      },
+
+      "google_translate_element",
+    );
+  }
